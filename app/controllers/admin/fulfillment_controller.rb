750b8250f4359748e2250cf6c73f960c036c7765
Kent Slaymaker <kent@1bg.com>
2019-12-01 09:57:34 -0700

    [rubocop] move logic to service

--- app/controllers/admin/fulfillment_controller.rb
+++ app/controllers/admin/fulfillment_controller.rb
@@ -2,6 +2,6 @@ class Admin::FulfillmentController < ApplicationController
   before_action :logged_in_as_admin?
 
   def index
-    @fulfillment = Order.fulfillment
+    @fulfillment = GetOrderFulfillment.call
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -17,6 +17,14 @@ class Order < ActiveRecord::Base
 
   scope :active, -> { where(fulfilled_on: nil).order('delivery_date asc') }
 
+  def self.to_be_fulfilled(day)
+    if day == Date.today
+      Order.where('fulfilled_on is NULL AND delivery_date <= ?', Date.today)
+    else
+      Order.where(fulfilled_on: nil, delivery_date: day)
+    end
+  end
+
   def self.display_all(show)
     display(Order.all, show)
   end
@@ -53,37 +61,5 @@ class Order < ActiveRecord::Base
 
   def unfulfill
     self.fulfilled_on = nil
-  end
-
-  def self.fulfillment
-    result = {}
-    week   = Date.today...Date.today + 7
-
-    week.each do |day|
-      result[day] = {}
-
-      Product.where(in_production: true).each do |product|
-        result[day][product.name] = {}
-        LineItem::SIZES.each do |size|
-          total = 0
-
-          Order.to_be_fulfilled(day).each do |order|
-            li                  = order.line_items.where(size: size, product_id: product.id, fulfilled: false)
-            li.map { |l| total += l.units }
-          end
-          result[day][product.name][size] = total
-        end
-      end
-    end
-
-    result
-  end
-
-  def self.to_be_fulfilled(day)
-    if day == Date.today
-      Order.where('fulfilled_on is NULL AND delivery_date <= ?', Date.today)
-    else
-      Order.where(fulfilled_on: nil, delivery_date: day)
-    end
   end
 end
